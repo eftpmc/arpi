a983b611555184074486b4cd37eed0b610ec18d0
eftpmc <zackhhi@gmail.com>
2023-06-09 23:39:20 -0400

    Gogo anime

--- index.js
+++ index.js
@@ -10,39 +10,37 @@ app.use(express.json());
 // Define your routes and handlers here
 app.get('/', async (req, res) => res.send("<h1>api 😾</h1>"));
 
-app.get('/api/zoro/search/:keyword', async (req, res) => {
-    try {
-      const { keyword } = req.params;
-      const searchUrl = `https://zoro.to/search?keyword=${encodeURIComponent(keyword)}`;
-  
-      const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        headless: true,
-      });
-      
-      const page = await browser.newPage();
-      await page.goto(searchUrl);
-  
-      // Your web scraping logic here
-      const data = await page.evaluate(() => {
-        // Extract the necessary information from the page
-        const animeList = Array.from(document.querySelectorAll('.flw-item'));
-        return animeList.map((anime) => ({
-          title: anime.querySelector('.film-name a').textContent.trim(),
-          image: anime.querySelector('.film-poster img').getAttribute('src'),
-          url: anime.querySelector('.film-name a').getAttribute('href'),
-        }));
-      });
-  
-      await browser.close();
-  
-      res.json(data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'An error occurred while scraping the website' });
-    }
-  });
+app.get('/api/gogo/search/:keyword', async (req, res) => {
+  try {
+    const { keyword } = req.params;
+    const searchUrl = `https://gogoanime.hu/search.html?keyword=${encodeURIComponent(keyword)}`;
+
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: true,
+    });
+
+    const page = await browser.newPage();
+    await page.goto(searchUrl);
+
+    // Your web scraping logic here
+    const data = await page.evaluate(() => {
+      // Extract the necessary information from the page
+      const animeList = Array.from(document.querySelectorAll('.items li'));
+      return animeList.map((anime) => ({
+        url: `https://gogoanime.hu${anime.querySelector('.name a').getAttribute('href')}`,
+        title: anime.querySelector('.name a').textContent.trim(),
+      }));
+    });
+
+    await browser.close();
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while scraping the website' });
+  }
+});
 
 // Start the server
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
