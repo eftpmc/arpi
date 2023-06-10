606a5ddbcb26553eb7026b887ad058d5ddda8539
eftpmc <zackhhi@gmail.com>
2023-06-09 22:27:07 -0400

    18.0.0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 
 const app = express();
 const port = 3000;
@@ -11,38 +11,38 @@ app.use(express.json());
 app.get('/', async (req, res) => res.send("<h1>api 😾</h1>"));
 
 app.get('/api/zoro/search/:keyword', async (req, res) => {
-  try {
-    const { keyword } = req.params;
-    const searchUrl = `https://zoro.to/search?keyword=${encodeURIComponent(keyword)}`;
-
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      headless: true,
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable',
-    });
-
-    const page = await browser.newPage();
-    await page.goto(searchUrl);
-
-    // Your web scraping logic here
-    const data = await page.evaluate(() => {
-      // Extract the necessary information from the page
-      const animeList = Array.from(document.querySelectorAll('.flw-item'));
-      return animeList.map((anime) => ({
-        title: anime.querySelector('.film-name a').textContent.trim(),
-        image: anime.querySelector('.film-poster img').getAttribute('src'),
-        url: anime.querySelector('.film-name a').getAttribute('href'),
-      }));
-    });
-
-    await browser.close();
-
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred while scraping the website' });
-  }
-});
+    try {
+      const { keyword } = req.params;
+      const searchUrl = `https://zoro.to/search?keyword=${encodeURIComponent(keyword)}`;
+  
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: true,
+      });
+      
+      const page = await browser.newPage();
+      await page.goto(searchUrl);
+  
+      // Your web scraping logic here
+      const data = await page.evaluate(() => {
+        // Extract the necessary information from the page
+        const animeList = Array.from(document.querySelectorAll('.flw-item'));
+        return animeList.map((anime) => ({
+          title: anime.querySelector('.film-name a').textContent.trim(),
+          image: anime.querySelector('.film-poster img').getAttribute('src'),
+          url: anime.querySelector('.film-name a').getAttribute('href'),
+        }));
+      });
+  
+      await browser.close();
+  
+      res.json(data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while scraping the website' });
+    }
+  });
 
 // Start the server
-app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
