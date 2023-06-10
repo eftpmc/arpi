791c87c6bfa5e4fbe403cc7c4026bd7231ed90b4
eftpmc <zackhhi@gmail.com>
2023-06-09 22:08:21 -0400

    New launch options

--- index.js
+++ index.js
@@ -16,11 +16,10 @@ app.get('/api/zoro/search/:keyword', async (req, res) => {
       const searchUrl = `https://zoro.to/search?keyword=${encodeURIComponent(keyword)}`;
   
       const browser = await puppeteer.launch({
-        args: [
-          "--no-sandbox",
-          "--disable-setuid-sandbox",
-        ]
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: true,
       });
+      
       const page = await browser.newPage();
       await page.goto(searchUrl);
   
