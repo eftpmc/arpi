dd8b85b5d8692e98b405eff3561f31e525bc9ca0
eftpmc <zackhhi@gmail.com>
2023-06-09 21:13:44 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Define your routes and handlers here
+app.get('/api/zoro/search/:keyword', async (req, res) => {
+    try {
+      const { keyword } = req.params;
+      const searchUrl = `https://zoro.to/search?keyword=${encodeURIComponent(keyword)}`;
+  
+      const browser = await puppeteer.launch({ headless: "new" });
+      const page = await browser.newPage();
+      await page.goto(searchUrl);
+  
+      // Your web scraping logic here
+      const data = await page.evaluate(() => {
+        // Extract the necessary information from the page
+        const animeList = Array.from(document.querySelectorAll('.flw-item'));
+        return animeList.map((anime) => ({
+          title: anime.querySelector('.film-name a').textContent.trim(),
+          image: anime.querySelector('.film-poster img').getAttribute('src'),
+          url: anime.querySelector('.film-name a').getAttribute('href'),
+        }));
+      });
+  
+      await browser.close();
+  
+      res.json(data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while scraping the website' });
+    }
+  });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
