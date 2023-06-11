c802881dd34799a557c9d64e32287a01911e0146
eftpmc <zackhhi@gmail.com>
2023-06-11 13:28:27 -0400

    Foundations

---
+++ api/gogo/search.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const puppeteer = require('puppeteer-extra');
+
+// Add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
+const Adblocker = AdblockerPlugin({
+  blockTrackers: true, // default: false
+});
+puppeteer.use(Adblocker);
+
+const router = express.Router();
+
+router.get('/:keyword', async (req, res) => {
+  try {
+    const { keyword } = req.params;
+    const searchUrl = `https://gogoanime.hu/search.html?keyword=${encodeURIComponent(keyword)}`;
+
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: true,
+    });
+
+    const page = await browser.newPage();
+    await page.goto(searchUrl);
+
+    const data = await page.evaluate(() => {
+      const animeList = Array.from(document.querySelectorAll('.items li'));
+      return animeList.map((anime) => ({
+        title: anime.querySelector('.name a').textContent.trim(),
+        url: `https://gogoanime.hu/${anime.querySelector('.name a').getAttribute('href').replace(/^\/category\//, '')}-episode-1`,
+        id: anime.querySelector('.name a').getAttribute('href').replace(/^\/category\//, ''),
+      }));
+    });
+
+    await browser.close();
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while scraping the website' });
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
-
 const app = express();
 const port = 3000;
 
@@ -8,39 +6,13 @@ const port = 3000;
 app.use(express.json());
 
 // Define your routes and handlers here
-app.get('/', async (req, res) => res.send("<h1>api 😾</h1>"));
+app.get('/', (req, res) => res.send("<h1>api 😾</h1>"));
 
-app.get('/api/gogo/search/:keyword', async (req, res) => {
-  try {
-    const { keyword } = req.params;
-    const searchUrl = `https://gogoanime.hu/search.html?keyword=${encodeURIComponent(keyword)}`;
+// Load routes
+const searchRoutes = require('./api/gogo/search');
 
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      headless: true,
-    });
-
-    const page = await browser.newPage();
-    await page.goto(searchUrl);
-
-    // Your web scraping logic here
-    const data = await page.evaluate(() => {
-      // Extract the necessary information from the page
-      const animeList = Array.from(document.querySelectorAll('.items li'));
-      return animeList.map((anime) => ({
-        url: `https://gogoanime.hu${anime.querySelector('.name a').getAttribute('href')}`,
-        title: anime.querySelector('.name a').textContent.trim(),
-      }));
-    });
-
-    await browser.close();
-
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred while scraping the website' });
-  }
-});
+// Register routes
+app.use('/api/gogo/search', searchRoutes);
 
 // Start the server
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
