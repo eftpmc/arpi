d04cc8da8dff0f20a3dd97d479699798c73b96a0
eftpmc <zackhhi@gmail.com>
2023-06-24 22:21:39 -0400

    episode id fix?

--- api/anime/zoro/watch.js
+++ api/anime/zoro/watch.js
@@ -20,8 +20,6 @@ router.get('/:id/:ep', async (req, res) => {
 
     $('.ssl-item.ep-item').each((index, element) => {
       const $element = $(element);
-      const episodeNumber = $element.data('number');
-      const episodeId = $element.data('id');
       const watchLink = $element.attr('href');
       const episodeIdFromLink = watchLink.split('=')[1];
 
